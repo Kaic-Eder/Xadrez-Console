3cea447fe2bba80f5a0fbec0498b4f1425fa2d47
Kaic Eder <kaic.eder@hotmail.com>
2024-08-12 01:32:39 -0300

    Implementando as restrições de movimento

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,27 +10,45 @@ internal class Program {
 
 			PartidaDeXadrez partida = new PartidaDeXadrez();
 
-			while (!partida.terminada) {
-				Console.Clear();
-				Tela.imprimirTabuleiro(partida.tab);
+            while (!partida.terminada) {
 
-				Console.WriteLine();
+                try {
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
 
-				Console.Write("Origem: ");
-				Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.WriteLine(" ");
+                        Console.WriteLine("turno: " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
-				bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-				Console.Clear() ;
-				Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                Console.WriteLine();
-                Console.Write("Destino: ");
-				Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.WriteLine();
 
-				partida.executaMovimento(origem, destino);
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaodeOrigem(origem);
 
-			}
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+
+
+                } catch (TabuleiroException e) {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
 
 			
 
